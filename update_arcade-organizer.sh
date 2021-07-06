a4417d9349bff977e959503b793a044571cdd6f4
José Manuel Barroso Galindo <theypsilon@gmail.com>
2021-07-06 20:31:11 +0200

    Redirecting to current user

--- update_arcade-organizer.sh
+++ update_arcade-organizer.sh
@@ -97,7 +97,7 @@ echo ""
 
 echo "Downloading the most recent _arcade-organizer script."
 echo " "
-curl ${CURL_RETRY} ${SSL_SECURITY_OPTION} --location -o /tmp/_arcade-organizer.sh https://raw.githubusercontent.com/MAME-GETTER/_arcade-organizer/master/_arcade-organizer.sh
+curl ${CURL_RETRY} ${SSL_SECURITY_OPTION} --location -o /tmp/_arcade-organizer.sh https://raw.githubusercontent.com/theypsilon/_arcade-organizer/master/_arcade-organizer.sh
 echo
 
 chmod +x /tmp/_arcade-organizer.sh
