f5d12b2f62014cdfbdf453b9a033cefdf697176b
José Manuel Barroso Galindo <theypsilon@gmail.com>
2025-03-19 11:58:52 +0100

    New reference to MAD db

--- _arcade-organizer.sh
+++ _arcade-organizer.sh
@@ -123,7 +123,7 @@ def make_config():
     ini_parser.initialize()
 
     config = dict()
-    config['MAD_DB'] = ini_parser.get_string('MAD_DB', "https://raw.githubusercontent.com/MiSTer-devel/MiSTer_ArcadeDatabase/db/mad_db.json.zip")
+    config['MAD_DB'] = ini_parser.get_string('MAD_DB', "https://raw.githubusercontent.com/MiSTer-devel/ArcadeDatabase_MiSTer/db/mad_db.json.zip")
     if config['MAD_DB'] == "https://raw.githubusercontent.com/misteraddons/MiSTer_Arcade_MAD/db/mad_db.json.zip":
         config['MAD_DB'] = "https://raw.githubusercontent.com/theypsilon/misteraddons_Arcade_MAD/db/mad_db.json.zip"
     config['MRADIR'] = ini_parser.get_string('MRADIR', "/media/fat/_Arcade/")
